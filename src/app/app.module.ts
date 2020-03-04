59e636c4c5bdceb04064d9bf561373d5ff510ddd
Redines <pontus.g91@gmail.com>
2020-03-04 17:49:39 +0100

    started working on dark theme, added footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MovieCardComponent
+    MovieCardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
